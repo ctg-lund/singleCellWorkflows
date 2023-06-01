da4d7c501ff411da40739f1ae46e1f974e191b97
Fattigman <jacob.karlstrom@gmail.com>
2023-06-01 14:08:02 +0200

    added script to download all the refererences

---
+++ bin/download-references.sh
@@ -0,0 +1,12 @@
+# VDJ Human reference
+wget https://cf.10xgenomics.com/supp/cell-vdj/refdata-cellranger-vdj-GRCh38-alts-ensembl-7.1.0.tar.gz
+# VDJ Mouse reference
+wget https://cf.10xgenomics.com/supp/cell-vdj/refdata-cellranger-vdj-GRCm38-alts-ensembl-7.0.0.tar.gz
+# GRCh38 human transcriptome reference
+wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-GRCh38-2020-A.tar.gz
+# GRCm38 mouse transcriptome reference
+wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-mm10-2020-A.tar.gz
+# GRCh38 human arc reference
+wget https://cf.10xgenomics.com/supp/cell-arc/refdata-cellranger-arc-GRCh38-2020-A-2.0.0.tar.gz
+# GRCm38 mouse arc reference
+wget https://cf.10xgenomics.com/supp/cell-arc/refdata-cellranger-arc-mm10-2020-A-2.0.0.tar.gz

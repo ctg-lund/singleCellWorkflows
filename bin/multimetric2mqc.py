a417329779c7c6d10b2158a6dacff44828ac07e9
Fattigman <jacob.karlstrom@gmail.com>
2023-06-02 14:30:06 +0200

    python script for parsing multi metrics into json

---
+++ bin/multimetric2mqc.py
@@ -0,0 +1,45 @@
+import csv
+import json
+import sys
+
+# Check if input file name was provided
+if len(sys.argv) < 2:
+    print('Usage: python convert.py file.csv')
+    sys.exit(1)
+
+# Get input file name from command-line argument
+input_file = sys.argv[1]
+
+# Initialize dictionaries for each category
+cells = {}
+library = {}
+other = {}
+
+# Open CSV file
+with open(input_file, 'r') as f:
+    # Create CSV reader
+    reader = csv.reader(f)
+
+    # Skip header row
+    next(reader)
+
+    # Read CSV file row by row
+    for row in reader:
+        # Extract values from row
+        category, library_type, grouped_by, group_name, metric_name, metric_value = row
+
+        # Add metric_name and metric_value to appropriate dictionary based on category
+        if category == 'Cells':
+            cells[metric_name] = metric_value
+        elif category == 'Library':
+            library[metric_name] = metric_value
+        else:
+            other[metric_name] = metric_value
+
+# Write dictionaries to JSON files
+with open('cells.json', 'w') as f:
+    json.dump(cells, f, indent=2)
+with open('library.json', 'w') as f:
+    json.dump(library, f, indent=2)
+with open('other.json', 'w') as f:
+    json.dump(other, f, indent=2)
